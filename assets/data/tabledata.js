f5d72328182caea634c1457767eddac4f5a1e657
f.aldawish <f.aldawish@tamkeentech.sa>
2020-03-25 16:35:47 +0300

    first commit

---
+++ assets/data/tabledata.js
@@ -0,0 +1,230 @@
+var saData = [
+    {
+        "title": "Riyadh",
+        "confirmed": 364,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 24.774265,
+        "longitude": 46.738586,
+        "total": 364
+
+    },
+    {
+        "title": "Qassim",
+        "confirmed": 1,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 26.094088	,
+        "longitude": 43.973454,
+        "total": 1
+    },
+    {
+        "title": "Najran",
+        "confirmed": 13,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 17.5656,
+        "longitude": 44.2289,
+        "total": 13
+    },
+    {
+        "title": "Jazan",
+        "confirmed": 7,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 16.909683,
+        "longitude": 42.567902,
+        "total": 7
+    },
+    {
+        "title": "Asir",
+        "confirmed": 1,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 19.0969,
+        "longitude": 42.8638,
+        "total": 1
+    },
+    {
+        "title": "Hafar Al Batin",
+        "confirmed": 1,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 28.446959,
+        "longitude": 	45.948944,
+        "total": 1
+    },
+    {
+        "title": "Tabuk",
+        "confirmed": 1,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 28.3835,
+        "longitude": 36.5662,
+        "total": 1
+    },
+    {
+        "title": "Nariyah",
+        "confirmed": 1,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 27.4704,
+        "longitude": 48.4855,
+        "total": 1
+    },
+    {
+        "title": "Damam",
+        "confirmed": 58,
+        "deaths": 0,
+        "recovered": 1,
+        "latitude": 26.396790,
+        "longitude": 50.140400,
+        "total": 59
+    },
+    {
+        "title": "Al Qatif",
+        "confirmed": 68,
+        "deaths": 0,
+        "recovered": 2,
+        "latitude": 26.565191	,
+        "longitude": 49.996376,
+        "total": 70
+    },    {
+        "title": "Jubail",
+        "confirmed": 68,
+        "deaths": 0,
+        "recovered": 2,
+        "latitude": 26.9598,
+        "longitude": 49.5687,
+        "total": 70
+    },
+    {
+        "title": "Dharan",
+        "confirmed": 20,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 26.288769,
+        "longitude":	50.114101,
+        "total": 20
+    },
+    {
+        "title": "Khobar",
+        "confirmed": 11,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 26.2172,
+        "longitude": 50.1971,
+        "total": 11
+
+    },
+    {
+        "title": "Al Ahasa",
+        "confirmed": 7,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 23.1670,
+        "longitude": 49.3653,
+        "total": 7
+    },
+    {
+        "title": "Hofuf",
+        "confirmed": 2,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 25.3800,
+        "longitude": 49.5888,
+        "total": 2
+    },
+    {
+        "title": "Abha",
+        "confirmed": 8,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 18.2465,
+        "longitude": 42.5117,
+        "total": 8
+    },
+    {
+        "title": "Al Qunfudhah",
+        "confirmed": 1,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 19.1281,
+        "longitude": 41.0787,
+        "total": 1
+    },
+    {
+        "title": "Mahayel Aseer",
+        "confirmed": 1,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 18.5392,
+        "longitude": 42.0696,
+        "total": 1
+    },
+    {
+        "title": "Bishah",
+        "confirmed": 14,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 19.9764,
+        "longitude": 42.5902,
+        "total": 14
+    },
+    {
+        "title": "Al Bahah",
+        "confirmed": 13,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 20.0217,
+        "longitude": 41.4713,
+        "total": 13
+    },
+    {
+        "title": "Taif",
+        "confirmed": 6,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 21.437273	,
+        "longitude": 40.512714,
+        "total": 6 
+
+    },
+    {
+        "title": "Makkah",
+        "confirmed": 153,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 21.422510,
+        "longitude": 39.826168,
+        "total": 153
+    },
+    {
+        "title": "Jeddah",
+        "confirmed": 134,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 21.543333,
+        "longitude": 39.172779,
+        "total": 134
+    },
+    {
+        "title": "Al-Madina",
+        "confirmed": 8,
+        "deaths": 0,
+        "recovered": 1,
+        "latitude": 24.470901,
+        "longitude": 	39.612236,
+        "total": 9
+    },
+    {
+        "title": "Arar",
+        "confirmed": 2,
+        "deaths": 0,
+        "recovered": 0,
+        "latitude": 30.9599,
+        "longitude": 41.0596,
+        "total": 2
+    }];
+
+
